341f75c57d8a6a0253f5ebfdebae0bb9348a058b
walkerZyy <1073040046@qq.com>
2018-04-23 10:33:04 +0800

    1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ##
 #基于react-boilerplate，当然，没有用国际化的部分。
-#虽然style-components很好，还是加了另外的UI库antd，以及用less辅助
+#虽然style-components很好，还是加了另外的UI库antd，以及用less辅助（主要用于粘贴一些现成的东西，或定义全局样式）
 ##
--- app/containers/HomePage/FooterContent.js
+++ app/containers/HomePage/FooterContent.js
@@ -7,7 +7,7 @@ const Content = styled.div`
 `;
 
 const FooterContent = function () {
-  const github = 'http://www.baidu.com';
+  const github = 'https://github.com/WalkerZyy/practise-of-react-boilerplate-try';
   return (
     <Content href={github} target="_blank">
       <a>github地址: {github}</a>
